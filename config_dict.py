2f2d0f389fbc31ebbeb03fdc6f97f52f9e1c710b
vinayak bakshi <vinayak.kayaniv91@gmail.com>
2020-04-22 22:39:09 -0400

    handling exceptions in config dict and creating custom exception

--- config_dict.py
+++ config_dict.py
@@ -4,11 +4,17 @@ class ConfigDict(dict):
 
     def __init__(self, filename):
         self._filename = filename
-        if os.path.isfile(self._filename):
-            with open(self._filename) as f:
-                for line in f.readlines():
-                    key,value = line.rstrip().split("=", 1)
-                    dict.__setitem__(self, key, value)            
+        if not os.path.isfile(self._filename):
+            try:
+                open(self._filename, 'w').close()
+            except IOError:
+                badpath = '\\'.join(self._filename.split("\\")[:-1])
+                raise IOError("pathname: {} is invalid".format(badpath))
+        with open(self._filename) as f:
+            for line in f.readlines():
+                key,value = line.rstrip().split("=", 1)
+                dict.__setitem__(self, key, value)
+
     
     def __setitem__(self, key, value):
 
@@ -18,3 +24,18 @@ class ConfigDict(dict):
                 line = '{0}={1}\n'.format(k, v)
                 f.write(line)
     
+    def __getitem__(self, key):
+        if key not in self.keys():
+            raise ConfigKeyError(self, key)
+        return dict.__getitem__(self, key)
+
+
+class ConfigKeyError(KeyError):
+
+    def __init__(self, inputDict, key):
+        self.key = key
+        self.keys = inputDict.keys()
+    
+    def __str__(self):
+        return "key {0} not found. Available keys: ({1})".format(self.key, ', '.join(self.keys))
+        
