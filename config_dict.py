7739efe1de7b8d9466f686be3a0d6dda94bcaadc
vinayak bakshi <vinayak.kayaniv91@gmail.com>
2020-04-23 23:07:59 -0400

    working with pickle files in config class

--- config_dict.py
+++ config_dict.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 class ConfigDict(dict):
 
@@ -29,6 +30,28 @@ class ConfigDict(dict):
             raise ConfigKeyError(self, key)
         return dict.__getitem__(self, key)
 
+class ConfigPickleDict(dict):
+
+    config_directory = './configs/'
+
+    def __init__(self, filename):
+        self._filename = os.path.join(ConfigPickleDict.config_directory, filename + '.pickle')
+        if (not os.path.isfile(self._filename)):
+            with open(self._filename, 'wb') as fh:
+                pickle.dump({}, fh)
+        with open(self._filename, 'rb') as fh:
+            pkl = pickle.load(fh)
+            self.update(pkl)
+    
+    def __setitem__(self, key, value):
+        dict.__setitem__(self, key, value)
+        with open(self._filename, 'wb') as fh:
+            pickle.dump(self, fh)
+    
+    def __getitem__(self, key):
+        if key not in self.keys():
+            raise ConfigKeyError(self, key)
+        return dict.__getitem__(self, key)
 
 class ConfigKeyError(KeyError):
 
@@ -38,4 +61,12 @@ class ConfigKeyError(KeyError):
     
     def __str__(self):
         return "key {0} not found. Available keys: ({1})".format(self.key, ', '.join(self.keys))
-        
+
+if __name__ == "__main__":
+    a = {
+        'username':'vinayak',
+        'kerberos':'bakshv'
+    }
+    pickle.dump(a, open('./configs/conf_test.pickle', 'wb'))
+    b = ConfigPickleDict('conf_test')
+    print(b)
